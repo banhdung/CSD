710848023907b2959c5ff8fba8d76d6b659608e3
Your Name <you@example.com>
2022-06-02 13:45:20 +0700

    CSD

---
+++ SU22_SE1641_CSD/src/Listobj/Node.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listobj;
+
+/**
+ *
+ * @author admin
+ */
+public class Node {
+    Person info;
+    Node next;
+    
+    Node(String name, int age) {
+        info = new Person(name, age);
+        next = null;
+    }
+    
+}
